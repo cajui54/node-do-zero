ce464e5d0df725b7e073907b73af4655ec9a4840
Jackson Silva <cajui54@gmail.com>
2024-01-29 10:43:51 -0300

    test project

---
+++ server.js
@@ -0,0 +1,67 @@
+/*import {createServer} from 'node:http';
+
+const server = createServer((request, response) => {
+    response.write('<h1>xx</h1>');
+
+    return response.end();
+});
+
+server.listen(3333);*/
+//import DatabaseMemory from './database-memory.js';
+import { fastify } from 'fastify';
+import DatabasePostgres from './database-postgres.js';
+
+const server = fastify();
+
+//const database = new DatabaseMemory();
+// POST: http://localhost:3333/videos
+const database = new DatabasePostgres();
+
+server.post('/videos', async (request, reply) => {
+    const {title, description, duration} = request.body;
+
+    await database.create({
+        title,
+        description,
+        duration
+    });
+    
+    return reply.status(201).send(`The datas ${title} has been insert in DB`);
+});
+server.get('/videos', async (request) => {
+    const search = await request.query.search;
+
+    const videos = await database.list(search);
+
+    return videos;
+})
+
+server.put('/videos/:id', async (request, reply) => {
+    const videoId = request.params.id;
+    
+    const {title, description, duration} = request.body;
+
+    await database.update(videoId, {
+        title,
+        description,
+        duration
+    });
+
+    return reply.status(204).send();
+})
+
+server.delete('/videos/:id', async (request, reply) => {
+    const videoId = request.params.id;
+
+    await database.delete(videoId);
+    return reply.status(204).send('Deletou!');
+})
+
+
+server.listen({
+    port: process.env.PORT
+});
+
+
+
+
